03f68d1756d5315bb0f5be44a569a2cbbf31c9c4
Alhuin <jjanin-r@student.42lyon.fr>
2022-10-05 13:53:44 +0200

    Update source.c

---
+++ level03/source.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <string.h>
+#include <time.h>
+#include <stdlib.h>
+
+void  decrypt(diff)
+{
+      int nb1 = 1971092817;
+      int nb2 = 1734767456;
+      int nb3 = 2070311806;
+      int nb4 = 862026877;
+      char c = 0;                                       // str = "Q}|u`sfg~sf{}|a3"
+
+      int len;
+      int i;
+
+      len = strlen(&nb1);                               // strlen(str)
+      i = 0;
+
+      while (i < len) {
+        *(&nb1 + i) = *(&nb1 + i) ^ diff                // str[i] = str[i] ^ diff
+        i++;
+      }
+
+      if (strncmp(&nb1, "Congratulations", 17) == 0) {  // if str == "Congratulations"
+          system("/bin/sh");
+      }
+      else {
+        puts("\nInvalid Password");
+      }
+
+      return ;
+}
+
+void  test(int input, int number)
+{
+	    int diff;
+	
+      diff = number - input;
+      if (diff > 21) {
+        return(decrypt(rand()));
+      }
+      return(decrypt(diff));
+}
+
+int   main(void)
+{
+      int input;
+
+      srand(time(0));
+
+      puts("***********************************");
+      puts("*\t\tlevel03\t\t**");
+      puts("***********************************");
+      printf("Password:");
+
+      scanf("%d", &input);
+      test(input, 322424845);
+      return (0);
+}
