b232d96833436ab51fa4060790b1ad427573ce92
Alhuin <jjanin-r@student.42lyon.fr>
2022-10-05 14:09:33 +0200

    Update source.c

--- level03/source.c
+++ level03/source.c
@@ -5,56 +5,56 @@
 
 void  decrypt(diff)
 {
-      int nb1 = 1971092817;
-      int nb2 = 1734767456;
-      int nb3 = 2070311806;
-      int nb4 = 862026877;
-      char c = 0;                                       // str = "Q}|u`sfg~sf{}|a3" + nul
+    int nb1 = 1971092817;
+    int nb2 = 1734767456;
+    int nb3 = 2070311806;
+    int nb4 = 862026877;
+    char c = 0;                                       // str = "Q}|u`sfg~sf{}|a3" + nul
 
-      int len;
-      int i;
+    int len;
+    int i;
 
-      len = strlen(&nb1);                               // strlen(str)
-      i = 0;
+    len = strlen(&nb1);                               // strlen(str)
+    i = 0;
 
-      while (i < len) {
-        *(&nb1 + i) = *(&nb1 + i) ^ diff                // str[i] = str[i] ^ diff
+    while (i < len) {
+        *(&nb1 + i) = *(&nb1 + i) ^ diff              // str[i] = str[i] ^ diff
         i++;
-      }
+    }
 
-      if (strncmp(&nb1, "Congratulations", 17) == 0) {  // if str == "Congratulations"
-          system("/bin/sh");
-      }
-      else {
+    if (strncmp(&nb1, "Congratulations", 17) == 0) {  // if str == "Congratulations"
+        system("/bin/sh");
+    }
+    else {
         puts("\nInvalid Password");
-      }
+    }
 
-      return ;
+    return();
 }
 
 void  test(int input, int number)
 {
-	    int diff;
+    int diff;
 	
-      diff = number - input;
-      if (diff > 21) {
-        return(decrypt(rand()));
-      }
-      return(decrypt(diff));
+    diff = number - input;
+    if (diff > 21) {
+      return(decrypt(rand()));
+    }
+    return(decrypt(diff));
 }
 
 int   main(void)
 {
-      int input;
+    int input;
 
-      srand(time(0));
+    srand(time(0));
 
-      puts("***********************************");
-      puts("*\t\tlevel03\t\t**");
-      puts("***********************************");
-      printf("Password:");
+    puts("***********************************");
+    puts("*\t\tlevel03\t\t**");
+    puts("***********************************");
+    printf("Password:");
 
-      scanf("%d", &input);
-      test(input, 322424845);
-      return (0);
+    scanf("%d", &input);
+    test(input, 322424845);
+    return (0);
 }
